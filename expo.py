60a46e7cb4cd019fe0b6413ce05c815e8d9f66de
narwar0042 <142799395+narwar0042@users.noreply.github.com>
2023-08-29 12:39:58 +0530

    Add files via upload

---
+++ expo.py
@@ -0,0 +1,9 @@
+#input section
+num = int(input("Enter the number: "))
+power_num = int(input("Enter the power number : "))
+
+#logic section
+calculate_expo = num ** power_num
+
+#display section
+print("The calculated answer is ",calculate_expo)
