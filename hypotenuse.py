0f4e4c1be8f5bc7cfdd7c532f85669c78d04e531
narwar0042 <142799395+narwar0042@users.noreply.github.com>
2023-08-29 13:01:17 +0530

    Add files via upload

---
+++ hypotenuse.py
@@ -0,0 +1,9 @@
+#find hypotenuse
+b = eval(input("enter the base :"))
+p = eval(input("enter the perpendiculer :"))
+
+#logic
+h = (p**2 + b**2)**0.5
+
+#display
+print("The hypotnuse is ",h)
---
+++ valu_swap.py
@@ -0,0 +1,12 @@
+#input section
+num_1 = int(input("Enter the First Number: "))
+num_2 = int(input("Enter the Second Number: "))
+
+#logic section
+temp = num_1
+num_1 = num_2
+num_2 = temp
+
+#display seciton
+print("The First Number is ",num_1)
+print("The Second Number is ",num_2)
