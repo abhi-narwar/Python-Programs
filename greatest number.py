52c51eb2bb1fe43d6cd8d70762ae349766a633dc
narwar0042 <142799395+narwar0042@users.noreply.github.com>
2023-09-04 16:22:02 +0530

    greatest number

---
+++ greatest number.py
@@ -0,0 +1,12 @@
+# Input two numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Compare the two numbers
+if num1 > num2:
+    greatest = num1
+else:
+    greatest = num2
+
+# Print the greatest number
+print("The greatest number is:", greatest)
