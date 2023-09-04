7613d003f618bc7f9b1b0f636b1981373bde0a51
narwar0042 <142799395+narwar0042@users.noreply.github.com>
2023-09-04 16:22:58 +0530

    age detector

---
+++ age detector.py
@@ -0,0 +1,8 @@
+# Get the user's age as input
+age = int(input("Enter your age: "))
+
+# Check if the age is above 18
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote yet.")
