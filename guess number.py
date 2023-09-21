7267a013135f251f9443e2f3f5ec035c19e96266
Abhishek narwar <142799395+narwar0042@users.noreply.github.com>
2023-09-21 11:01:57 +0530

    Add files via upload

---
+++ guess number.py
@@ -0,0 +1,11 @@
+from random import*
+c_num=randrange(1,101)
+while 1:
+    u_num=int(input())
+    if u_num == c_num:
+        print("you win")
+        break
+    elif u_num>c_num:
+        print("too large")
+    else:
+        print("too small")
