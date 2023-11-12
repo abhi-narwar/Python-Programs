9307c0e456a1c4199bfcae541784dc0b2ff345a7
Abhishek narwar <142799395+narwar0042@users.noreply.github.com>
2023-11-12 19:27:42 +0530

    Add files via upload

---
+++ strange number.py
@@ -0,0 +1,19 @@
+n=int(input("Enter the no:"))
+st = 0
+for i in range (2,n):
+    if n%i==0:
+        flag=1
+        for j in range (2,i):
+            if i%j== 0 :
+                # print("Not a strange no ")
+                flag=0
+                break
+        if flag==0:
+            print("Not Strange")
+            break
+        st=i
+else:
+    if st>n**0.5:
+        print("strange no")
+    else:
+        print("Not a strange no ")
