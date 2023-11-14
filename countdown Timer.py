4507bae3e7c5ca2d02f623051064adb9f3d7e486
Abhishek narwar <142799395+narwar0042@users.noreply.github.com>
2023-11-14 17:18:15 +0530

    Add files via upload

---
+++ countdown Timer.py
@@ -0,0 +1,10 @@
+#1. Countdown Timer:
+ #Write a program that takes an integer input from the user and counts down from that number to 1 
+#using a while loop
+countdown_number=int(input("enter the number"))
+while countdown_number>0:
+    print(countdown_number)
+    countdown_number=countdown_number+1
+
+
+    
