f85c891fb58e5741e0cb44cddfb32812af43f7b3
Abhishek narwar <142799395+narwar0042@users.noreply.github.com>
2023-11-15 12:29:46 +0530

    Add files via upload

---
+++ using while loop password validation.py
@@ -0,0 +1,7 @@
+correct_password = "secure123"  # Replace this with your desired password
+entered_password = ""
+
+while entered_password != correct_password:
+    entered_password = input("Enter the password: ")
+
+print("Correct password entered. Access granted!")
