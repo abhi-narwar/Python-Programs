ff06cc0af1e3dd9d86caef8c1ae42e554286e355
Abhishek narwar <142799395+narwar0042@users.noreply.github.com>
2023-11-21 21:04:14 +0530

    Add files via upload

---
+++ multiply values in dict.py
@@ -0,0 +1,9 @@
+# Example dictionary
+my_dict = {'a': 2, 'b': 3, 'c': 4}
+
+# Multiply all values in the dictionary
+result = 1
+for value in my_dict.values():
+    result *= value
+
+print(f"The result of multiplying all values in the dictionary is: {result}")
