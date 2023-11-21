7f18dbec3dc73f2a0bc58944ae0ca946f4e7dada
Abhishek narwar <142799395+narwar0042@users.noreply.github.com>
2023-11-21 15:31:48 +0530

    Add files via upload

---
+++ merge dict.py
@@ -0,0 +1,7 @@
+info1={'name':'Abhishek','rollno':2,'college':'gla university','cpi':4.5}
+new_info={'name':'jatin','mobile':7834783748}
+
+for i in new_info:
+    info1[i]=new_info[i]
+
+print(info1)
