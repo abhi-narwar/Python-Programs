0c8fed4f10b7c5c3965aaec3cc4435e62060d90c
Abhishek narwar <142799395+narwar0042@users.noreply.github.com>
2023-11-21 21:21:54 +0530

    Add files via upload

--- multiply values in dict.py
+++ multiply values in dict.py
@@ -1,9 +1,7 @@
-# Example dictionary
-my_dict = {'a': 2, 'b': 3, 'c': 4}
+my_dict = {'b': 3, 'a': 2, 'c': 4}
 
-# Multiply all values in the dictionary
-result = 1
-for value in my_dict.values():
-    result *= value
 
-print(f"The result of multiplying all values in the dictionary is: {result}")
+sorted_dict = dict(sorted(my_dict.items()))
+
+
+print("Sorted Dictionary by Key:", sorted_dict)
