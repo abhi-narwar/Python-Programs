5f3a71a84ab06256458f8ca4ca2c6a3660f1f8b6
Abhishek narwar <142799395+narwar0042@users.noreply.github.com>
2023-11-24 20:38:29 +0530

    Add files via upload

---
+++ pangram or not using function .py
@@ -0,0 +1,10 @@
+def pangram(phrase):
+        letters={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'}
+        phrase=set(phrase)
+        if phrase>=letters:
+            return True
+        else:
+            return False
+print(pangram('abcde'))
+
+    
