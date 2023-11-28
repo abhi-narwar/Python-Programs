41ce670839d9f6baea174c224265aa11346a18e0
Abhishek narwar <142799395+narwar0042@users.noreply.github.com>
2023-11-28 23:37:46 +0530

    Add files via upload

---
+++ calculate area and perimeter of rectangle using function.py
@@ -0,0 +1,14 @@
+def area(a,b):
+    return a*b
+
+def perimeter(a,b):
+    return 2*(a+b)
+
+length=int(input("enter the length"))
+breadth=int(input("enter the breadth"))
+c=area(length,breadth)
+d=perimeter(length,breadth)
+
+print(c)
+print(d)
+
