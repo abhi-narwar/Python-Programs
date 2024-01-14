65f7d0753f3afe7fdda89b7b3f2e9cd6cbe0eb6e
Abhishek narwar <142799395+narwar0042@users.noreply.github.com>
2024-01-14 19:09:38 +0530

    Add files via upload

---
+++ student class.py
@@ -0,0 +1,14 @@
+class Student:
+    def __init__(self):
+        self.name='vishnu'
+        self.age=20
+        self.marks=900
+    def speak(self):
+        print('hi i am',self.name)
+        print('my age is ',self.age)
+        print('my marks are',self.marks)
+
+
+
+s1=Student()
+s1.speak()
