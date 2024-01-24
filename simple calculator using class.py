ecd132c9f1db296154496a34fe60d18853f414e0
Abhishek narwar <142799395+narwar0042@users.noreply.github.com>
2024-01-24 19:42:58 +0530

    Add files via upload

---
+++ simple calculator using class.py
@@ -0,0 +1,23 @@
+class calculator:
+    @staticmethod
+    def add(a,b):
+      return a+b
+    
+    @staticmethod
+    def subtract(a,b):
+      return a-b
+    
+    @staticmethod
+    def multiply(a,b):
+      return a*b
+    
+    @staticmethod
+    def divide(a,b):
+      return a//b
+a=12
+b=10
+print(calculator.add(a,b))
+print(calculator.subtract(a,b))
+print(calculator.multiply(a,b))
+print(calculator.divide(a,b))
+      
